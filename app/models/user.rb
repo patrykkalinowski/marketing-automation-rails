cf29a702ee553bb7090a9ac6c7081dbeba3fd486
Patryk Kalinowski <patryk.kalinowski@netguru.pl>
2016-06-23 12:48:45 +0200

    Timeline working with messages and events, need to fix ordering

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,65 @@ class User < ActiveRecord::Base
     # Tell the UserMailer to send a welcome email
     UserMailer.welcome_email(self).deliver_now
   end
+
+  def prepare_timeline
+    timeline = Array.new
+    # combine all events and messages to one array
+    timeline.concat(messages).concat(events)
+
+    # sort all events in timeline by time descending
+    sorted_timeline = timeline.sort_by { |key| key['time'] }.reverse
+  end
+
+  def events
+    events = Ahoy::Event.where(user_id: self)
+
+    events_array = Array.new
+    events.each do |event|
+      # TODO: extract and save info
+      events_array << {
+        action: "viewed page",
+        title: event.properties['title'],
+        url: event.properties['url'],
+        time: event.time
+        }
+      end
+
+      events_array
+  end
+
+  def messages
+    messages = Ahoy::Message.where(user_id: self)
+
+    messages_array = Array.new
+
+    messages.each do |message|
+      messages_array << {
+        action: "was delivered message",
+        title: message.subject,
+        url: nil,
+        time: message.sent_at
+      }
+
+      if message.opened_at
+        messages_array << {
+          action: "opened message",
+          title: message.subject,
+          url: nil,
+          time: message.opened_at
+        }
+      end
+
+      if message.clicked_at
+        messages_array << {
+          action: "clicked message",
+          title: message.subject,
+          url: nil,
+          time: message.clicked_at
+        }
+      end
+    end
+
+    messages_array
+  end
 end
