9a7b00236f416fb57faeb9daebd8ba92dce422cd
Patryk Kalinowski <patryk.kalinowski@netguru.pl>
2016-10-20 11:29:49 +0200

    Basic segment update draft

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -6,4 +6,7 @@ class SegmentsController < ApplicationController
   def show
     @segment = Segment.find params[:id]
   end
+
+  
+
 end
--- app/models/segment.rb
+++ app/models/segment.rb
@@ -2,24 +2,39 @@ class Segment < ActiveRecord::Base
   serialize :rules
   has_and_belongs_to_many :users
 
-  def update
-    type = self.rules[:rule][:type]
-    # events = Ahoy::Event.all
-    #
-    # events.each do |event|
-    #   if event.name == type
-    #     self.users << event.user_id
-    #   end
-    # end
+  def self.time
+    # check if event is newer than last segment update
+    last_update = Time.zone.now - 14.day
 
-    event = Ahoy::Event.find(3)
+    events = Ahoy::Event.all
 
-    if event.name == type
-      self.users << User.find(event.user_id)
+    events.each do |event|
+      puts event.time
+      puts event.time.between?(last_update, Time.zone.now)
+      puts "---"
     end
+  end
+
+  def self.update_segments
+    last_update = Time.zone.now - 300.day
+    puts last_update..Time.zone.now
+    new_events = Ahoy::Event.where(time: last_update..Time.zone.now)
+    # segments = Segment.find 6
+    seg = Segment.find 6
+
+    new_events.find_each do |event|
+      # segments.find_each do |segment|
+        if event.name == seg.rules[:rule][:type] && seg.rules[:rule][:properties][:page] == event.properties['page']
+        # self.users << event.user_id
+        seg.users << User.find(event.user_id)
+      end
+    end
+
   end
 
   def type
     type = self.rules[:rule][:type]
   end
+
+
 end
