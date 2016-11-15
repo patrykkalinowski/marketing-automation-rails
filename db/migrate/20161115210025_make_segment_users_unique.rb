879da69810cce6d75a79dfbe3dc62faa89936f85
Patryk Kalinowski <patryk.kalinowski@posteo.net>
2016-11-15 22:04:25 +0100

    segments_users are unique

---
+++ db/migrate/20161115210025_make_segment_users_unique.rb
@@ -0,0 +1,5 @@
+class MakeSegmentUsersUnique < ActiveRecord::Migration
+  def change
+    add_index :segments_users, [:segment_id, :user_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114205030) do
+ActiveRecord::Schema.define(version: 20161115210025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 20161114205030) do
     t.integer  "user_id",    null: false
     t.datetime "created_at"
   end
+
+  add_index "segments_users", ["segment_id", "user_id"], name: "index_segments_users_on_segment_id_and_user_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
